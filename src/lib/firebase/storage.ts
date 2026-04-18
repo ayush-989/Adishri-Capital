3138e1333466ad4a7a3aa0d178a24ecb405fba5a
ayush-989 <ayusht9189@gmail.com>
2026-04-18 21:54:44 +0530

    work

--- src/lib/firebase/storage.ts
+++ src/lib/firebase/storage.ts
@@ -5,4 +5,5 @@ export const uploadFile = async (file: File, path: string): Promise<string> => {
   const storageRef = ref(storage, path);
   const snapshot = await uploadBytes(storageRef, file);
   return await getDownloadURL(snapshot.ref);
+  // jsbdbcbx sbbcdjbsb
 };
